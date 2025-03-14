aedea7213e1aa176734e3a67f3ece399b9bfdc4f
aparajit0511 <aparajit35@gmail.com>
2025-03-15 02:00:30 +0530

    more codes

--- src/collections/CheckAnagram.java
+++ src/collections/CheckAnagram.java
@@ -1,2 +1,40 @@
-package collections;public class CheckAnagram {
+package collections;
+
+import java.util.HashMap;
+
+public class CheckAnagram {
+    public static void main(String[] args) {
+        String input1 = "dam";
+        String input2 = "mad";
+
+        HashMap<Character,Integer> word1 = new HashMap<>();
+        HashMap<Character,Integer> word2 = new HashMap<>();
+
+        for(int i = 0; i < input1.length(); i++){
+            char item = input1.charAt(i);
+            if(word1.containsKey(item)){
+                word1.put(item,word1.get(item)+1);
+            }
+            else{
+                word1.put(item,1);
+            }
+        }
+
+        for(int i = 0; i < input2.length(); i++){
+            char item = input2.charAt(i);
+            if(word2.containsKey(item)){
+                word2.put(item,word2.get(item)+1);
+            }
+            else{
+                word2.put(item,1);
+            }
+        }
+
+        if(word1.equals(word2)){
+            System.out.println("String is an anagram");
+        }
+        else{
+            System.out.println("String isnt an anagram");
+        }
+    }
 }
--- src/collections/FindCommonElements.java
+++ src/collections/FindCommonElements.java
@@ -1,2 +1,24 @@
-package collections;public class FindCommonElements {
+package collections;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class FindCommonElements {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,4,5};
+        int[] nums2 = {2,3,7,8,9};
+
+        HashSet<Integer> numsHash = new HashSet<>();
+        
+        for(int i=0; i<nums1.length;i++){
+            numsHash.add(nums1[i]);
+        }
+        
+        for(int i=0;i< nums2.length;i++){
+            if(numsHash.contains(nums2[i])){
+                System.out.println(nums2[i]);
+            }
+        }
+    }
 }
